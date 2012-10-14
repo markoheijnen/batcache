c8fcbe450759d374a8a23d7b8dd5c6791a9b57f3
Marko Heijnen <info@markoheijnen.com>
2012-10-14 21:48:20 +0200

    Cleanup batcache file and check if $post did exists
    Sometimes after removing a menu item the clean_post_cache is getting
    called with a non existing $post_id

--- batcache.php
+++ batcache.php
@@ -9,24 +9,24 @@ Version: 1.0
 */
 
 // Do not load if our advanced-cache.php isn't loaded
-if ( ! is_object($batcache) || ! method_exists( $wp_object_cache, 'incr' ) )
+if ( ! is_object( $batcache ) || ! method_exists( $wp_object_cache, 'incr' ) )
 	return;
 
 $batcache->configure_groups();
 
 // Regen home and permalink on posts and pages
-add_action('clean_post_cache', 'batcache_post');
+add_action( 'clean_post_cache', 'batcache_post' );
 
 // Regen permalink on comments (TODO)
 //add_action('comment_post',          'batcache_comment');
 //add_action('wp_set_comment_status', 'batcache_comment');
 //add_action('edit_comment',          'batcache_comment');
 
-function batcache_post($post_id) {
+function batcache_post( $post_id ) {
 	global $batcache;
 
 	$post = get_post($post_id);
-	if ( $post->post_type == 'revision' || get_post_status($post_id) != 'publish' )
+	if ( ! $post || $post->post_type == 'revision' || get_post_status($post_id) != 'publish' )
 		return;
 
 	batcache_clear_url( get_option('home') );
@@ -34,12 +34,15 @@ function batcache_post($post_id) {
 	batcache_clear_url( get_permalink($post_id) );
 }
 
-function batcache_clear_url($url) {
+function batcache_clear_url( $url ) {
 	global $batcache;
-	if ( empty($url) )
+
+	if ( empty( $url ) )
 		return false;
-	$url_key = md5($url);
-	wp_cache_add("{$url_key}_version", 0, $batcache->group);
-	return wp_cache_incr("{$url_key}_version", 1, $batcache->group);
+
+	$url_key = md5( $url );
+	wp_cache_add( "{$url_key}_version", 0, $batcache->group );
+
+	return wp_cache_incr( "{$url_key}_version", 1, $batcache->group );
 }
 
